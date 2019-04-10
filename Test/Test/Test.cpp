3fb7960e8d7a0498d2eda7a96a96a538d069b639
matansg <>
2019-04-10 15:44:15 +0300

    VS stuff

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -2,32 +2,12 @@
 //
 
 #include <stdio.h>
-#include "../../wolfssl/wolfcrypt/aes.h"
+#include "../../wolfssl/wolfcrypt/rsa.h"
 
 int main()
 {
 
-	Aes aes;
-
-	#define LEN 48
-
-	unsigned char key[32] = { 0 };
-	unsigned char iv[16] = { 0 };
-	unsigned char plain[LEN] = { 0 };
-	unsigned char cipher[LEN];
-	int i;
-
-	for (i = 0; i < 32; i++)
-		key[i] = i & 0xFF;
-
-	for (i = 0; i < 16; i++)
-		iv[i] = i & 0xFF;
-
-	for (i = 0; i < LEN; i++)
-		plain[i] = (i*i) & 0xFF;
-
-	wc_AesSetKey(&aes, key, 32, iv, AES_DECRYPTION);
-	wc_AesCbcDecrypt(&aes, cipher, plain, LEN);
+	RsaKey key;
 
     return 0;
 }
